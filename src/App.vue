89f48ce58465c10c5fc522fa72180dc50c08205c
Jong <fgh1937@yahoo.com>
2023-04-05 21:07:58 +0900

    01 - Data Binding in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 :class="클래스명" :style="스타일"> {{ products[0] }} 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+  <div>
+    <h4> {{ products[1] }} 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+  <div>
+    <h4> {{ products[2] }} 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return  {
+      price1 : 60,
+      price2 : 70,
+      스타일 : 'color : blue',
+      클래스명 : 'wow',
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
