ae0d59fa9ce3c2205ab209addc057cfd6c9dbae1
Jong <fgh1937@yahoo.com>
2023-04-06 21:58:36 +0900

    v-if & Modal

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,31 @@
     <a v-for="i in 메뉴들" :key="i">{{ i }}</a>
   </div>
 
+  <div class="black-bg" v-if="모달창열렷니 == true">
+    <div class="white-bg">
+      <h4>상세페이지 임</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창열렷니=false">닫기</button>
+    </div>
+  </div>
+
   <div>
-    <h4>{{products[0]}}</h4>
+    <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렷니 = true">{{products[0]}}</h4>
     <p>50만원</p>
     <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{ 신고수[0] }}</span>
   </div>
   <div>
-    <h4>{{products[1]}}</h4>
+    <img src="./assets/room1.jpg" class="room-img">
+    <h4 @click="모달창열렷니 = true">{{products[1]}}</h4>
     <p>50만원</p>
-    <button @mouseover="신고수[1]++">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
+    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{ 신고수[1] }}</span>
   </div>
   <div>
-    <h4>{{products[2]}}</h4>
+    <img src="./assets/room2.jpg" class="room-img">
+    <h4 @click="모달창열렷니 = true">{{products[2]}}</h4>
     <p>50만원</p>
-    <button @click="increase">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
+    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{ 신고수[2] }}</span>
   </div>
 </template>
 
@@ -29,11 +40,7 @@ export default {
       신고수 : [0,0,0],
       products : ['역삼동원룸','천호동원룸','마포구원룸'],
       메뉴들 :  ['Home','Shop','About'],
-    }
-  },
-  methods :{
-    increase(){
-      this.신고수[2] += 1;
+      모달창열렷니 : false,
     }
   },
   components: {
@@ -42,6 +49,22 @@ export default {
 </script>
 
 <style>
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -58,4 +81,9 @@ export default {
   color : white;
   padding: 10px;
 }
+.room-img {
+  width: 100%;
+  margin-top : 40px
+}
+
 </style>
